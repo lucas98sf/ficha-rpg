4bbb2cd76ba2d2641ddc247e9c8ca7abbe5aefdc
HimLgQ <thejohans251@gmail.com>
2021-11-25 02:28:52 -0500

    aa

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Home</title>
       </Head>
       <h1>Home</h1>
-      <Link href={`https://mundo-sem-vida.herokuapp.com/players`}>
+      <Link href={`players`}>
           Players
         </Link>
     </div>
