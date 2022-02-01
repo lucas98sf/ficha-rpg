7a50a145bc8a488fba95f3c215cbd29df65b86e2
Duardo <thejohans251@gmail.com>
2022-02-01 12:19:59 -0500

    Loading

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,31 @@
-import '../public/styles.css'
+import "../public/styles.css";
+import React from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const router = useRouter();
+    const [pageLoading, setPageLoading] = React.useState(false);
+    React.useEffect(() => {
+        const handleStart = () => {
+            setPageLoading(true);
+        };
+        const handleComplete = () => {
+            setPageLoading(false);
+        };
+
+        router.events.on("routeChangeStart", handleStart);
+        router.events.on("routeChangeComplete", handleComplete);
+        router.events.on("routeChangeError", handleComplete);
+    }, [router]);
+
+    return pageLoading ? (
+        <div className="loading">
+            <Image src={`/images/load.gif`} alt="loading" width="500" height="300"/>
+        </div>
+    ) : (
+        <Component {...pageProps} />
+    );
 }
 
-export default MyApp
+export default MyApp;
